211895b9c88076fe1bc9c87268cd7489920b20e1
Rui Fang <ruifang@Ruis-MBP.guest.corp.microsoft.com>
2020-11-03 14:06:53 +0800

    Build plugin with JDK11 since intelliJ source code building JDK migration

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/util/MethodUtils.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/util/MethodUtils.java
@@ -22,12 +22,15 @@
 
 package com.microsoft.intellij.util;
 
-import com.intellij.openapi.module.*;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.module.ModuleTypeId;
 import com.intellij.openapi.project.Project;
 import com.microsoft.intellij.AzurePlugin;
 import com.microsoft.intellij.ui.libraries.AILibraryHandler;
 
 import java.io.File;
+
 import static com.microsoft.intellij.ui.messages.AzureBundle.message;
 
 public class MethodUtils {
